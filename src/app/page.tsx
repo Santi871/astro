0fa189043d58f4b68964fb6f7984182b96e2ab51
Santiago Vegega <santivegega@gmail.com>
2024-08-13 00:08:48 -0500

    chore: add tailwind prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-br from-black via-black to-indigo-800">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-gradient-to-br from-black via-black to-indigo-800 p-24">
       <header className="fixed top-0 w-screen p-10 text-center text-white">
         Asd
       </header>
-      <div className="flex h-1/2 w-1/2 flex-col gap-5 border text-white ml-96 mt-32">
+      <div className="gap-5flex-col ml-96 mt-32 flex h-1/2 w-1/2 border text-white">
         <h2 className="text-5xl font-extrabold">Hello, I'm Santiago</h2>
         <p className="font-semibold">
           I'm a Full Stack Engineer from Argentina with a passion for music,
